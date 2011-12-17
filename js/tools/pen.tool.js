d09844074a32d2daf2dfac59082960370a5fa5ec
Pawel Wilk <pwilkmielno@gmail.com>
2011-12-18 01:14:26 +0400

    improved pen

--- js/tools/pen.tool.js
+++ js/tools/pen.tool.js
@@ -3,21 +3,37 @@
   RaphaelPaint.addTool('pen', {
     name: 'Pen',
     start: { x: 0, y: 0 },
+    points: [],
+    path: null,
     onMouseMove: function (paper, event) {
       if (event.mouseButton == 1) {
-        var line = this.drawLine(paper, this.start.x, this.start.y, event.layerX, event.layerY);
-        this.start = { x: event.layerX, y: event.layerY };
-        return line;
+        if (this.path != null) {
+          this.path.remove();
+        }
+        this.points.push({x: event.layerX, y: event.layerY});
+        this.path = paper.path(this.linePath(this.start, this.points)).attr({stroke: '1px solid #000'});
       }
     },
     onMouseUp: function (paper, event) {
-      return this.drawLine(paper, this.start.x, this.start.y, event.layerX, event.layerY);
+      this.points.push({x: event.layerX, y: event.layerY});
+      this.path.remove();
+      var line = paper.path(this.linePath(this.start, this.points)).attr({stroke: '1px solid #000'});
+      this.points = [];
+      this.start = { x: 0, y: 0};
+      this.path = null;
+      return line;
     },
     onMouseDown: function (paper, event) {
       this.start = { x: event.layerX, y: event.layerY };
     },
-    drawLine: function (paper, startX, startY, endX, endY) {
-      return paper.path(['M',startX,startY,'L',endX,endY]).attr({stroke: '1px solid #000'});
+    linePath: function (start, points) {
+      var path = ['M', start.x, start.y];
+      for (var x in points) {
+        path.push('L');
+        path.push(points[x].x);
+        path.push(points[x].y);
+      }
+      return path;
     }
   });
 })();
