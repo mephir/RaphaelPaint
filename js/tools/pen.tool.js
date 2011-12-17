d811cdea782702923ae21933784c775919c115b2
Pawel Wilk <pwilkmielno@gmail.com>
2011-12-18 00:54:09 +0400

    fix for pen - make line continous

--- js/tools/pen.tool.js
+++ js/tools/pen.tool.js
@@ -2,13 +2,22 @@
 {
   RaphaelPaint.addTool('pen', {
     name: 'Pen',
+    start: { x: 0, y: 0 },
     onMouseMove: function (paper, event) {
       if (event.mouseButton == 1) {
-        paper.circle(event.layerX, event.layerY, 1).attr({fill: '#000', stroke: 'none'});
+        var line = this.drawLine(paper, this.start.x, this.start.y, event.layerX, event.layerY);
+        this.start = { x: event.layerX, y: event.layerY };
+        return line;
       }
     },
+    onMouseUp: function (paper, event) {
+      return this.drawLine(paper, this.start.x, this.start.y, event.layerX, event.layerY);
+    },
     onMouseDown: function (paper, event) {
-      paper.circle(event.layerX, event.layerY, 1).attr({fill: '#000', stroke: 'none'});
+      this.start = { x: event.layerX, y: event.layerY };
+    },
+    drawLine: function (paper, startX, startY, endX, endY) {
+      return paper.path(['M',startX,startY,'L',endX,endY]).attr({stroke: '1px solid #000'});
     }
   });
 })();
